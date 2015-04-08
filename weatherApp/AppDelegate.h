69372ca2899e40119decda9c9750cdf74003fc70
dbarabander <barabander@gmail.com>
2015-04-08 13:13:51 -0400

    Initial Commit

---
+++ weatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weatherApp
+//
+//  Created by Daniel Barabander on 4/8/15.
+//  Copyright (c) 2015 Tom Prezioso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weatherApp/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  weatherApp
+//
+//  Created by Daniel Barabander on 4/8/15.
+//  Copyright (c) 2015 Tom Prezioso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
